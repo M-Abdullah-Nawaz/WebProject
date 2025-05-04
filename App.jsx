3dc3a638c90a22a0c2a16b1f001ac494cc7ac294
Muhammad Abdullah Nawaz <108382189+M-Abdullah-Nawaz@users.noreply.github.com>
2025-05-04 23:02:04 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import LandingPage from "./pages/LandingPage";
+import TermsAndConditions from "./pages/T&cPage";
+import ContactUs from "./pages/ContactUSPage";
+import Settings from "./pages/settings1";
+
+function App() {
+  return (
+    <div className="App">
+     <Settings/>
+    </div>
+  );
+}
+export default App;
+
